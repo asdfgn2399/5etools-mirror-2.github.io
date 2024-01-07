45fe474f8e8c1382631c21bb6cf4a6cece78d5ac
asdfgn2399 <alexannett88@gmail.com>
2024-01-06 17:37:36 -0800

    v1.197.3.2

--- js/navigation.js
+++ js/navigation.js
@@ -938,7 +938,7 @@ class NavBar {
     <input id="popupEmail" type="text" placeholder="e.g. yourname@example.com"><br>
 		<label for="popupPassword">Password:</label><br>
     <input id="popupPassword" type="text" placeholder="e.g. h$kd9I8K4nb-D6r"><br>
-		<button onclick="NavBar.firebaseSignIn(popupEmail.value, popupPassword.value, ${isCreating})" class="btn bottom-right-button">${btnTxt}</button>`
+		<button id="signInButton" onclick="NavBar.firebaseSignIn(popupEmail.value, popupPassword.value, ${isCreating})" class="btn bottom-right-button">${btnTxt}</button>`
  
 	}
 
